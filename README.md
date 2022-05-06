7d4c8fc18c08f891b1765e29552e3a60f0821c4f
Daniel McNab <36049421+DJMcNab@users.noreply.github.com>
2022-05-06 10:01:17 +0100

    Start documenting and following API guidelines

--- README.md
+++ README.md
@@ -1,11 +1,61 @@
+# Declaration Site
+
+Implements iterating through the debug info associated with currently loaded
+objects, to find functions. The main entry point for this crate is
+[`for_some_currently_loaded_rust_functions`], which internally iterates
+through all functions which can be found.
+
+This crate was created to be used by [bevy](https://bevyengine.org/), for reporting errors
+with systems. Other crates making similar use of dependency injection may
+find this useful.
+This can be used to get the source code location of a function item type.
+This is implemented in [`declaration_by_name`] and the related functions.
+However, if searching for multiple names, it will be much more efficient to
+use [`for_some_currently_loaded_rust_functions`]:
+
+```rust,no_run
+# use declaration_site::{for_some_currently_loaded_rust_functions, DeclarationSite};
+# use std::collections::HashMap;
+let mut expected_names = HashMap::<String, Option<DeclarationSite>>::from([
+    // Note that source locations for std/core are less likely to work, this is just an example
+    ("std::io::read".into(), None),
+    ("std::io::write".into(), None),
+]);
+for_some_currently_loaded_rust_functions(|name, function| {
+    if let Some(result) = expected_names.get_mut(&name) {
+        // Note that the `TryFrom` impl is only for `&Function`, so need to add
+        // the reference
+        *result = DeclarationSite::try_from(&function).ok();
+    };
+    // We could bail early here if all names have been filled, but that would
+    // compilacate the example
+    None::<core::convert::Infallible>
+});
+// Do things with `expected_names`...
+```
+
+## Caveats
+
+This is a best-effort search only. It may fail to find a given name for any number
+of reasons:
+
+- Will not find anything on WASM.
+- If the function is not linked - which may occur if it is never called. it may not be found.
+- The function has been inlined
+- If running on MacOS (we currently silently fail for reasons unknown, and the author cannot debug this due to not having a way to run it. Contributions welcome!)
+
+## Changelog
+
+See [CHANGELOG.md](CHANGELOG.md)
+
 ## License
 
 Licensed under either of
 
 - Apache License, Version 2.0
-  ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+  ([LICENSE-APACHE](LICENSE-APACHE) or <http://www.apache.org/licenses/LICENSE-2.0>)
 - MIT license
-  ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+  ([LICENSE-MIT](LICENSE-MIT) or <http://opensource.org/licenses/MIT>)
 
 at your option.
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,88 @@
-//! ## Caveats
-//!
-//! This is a best-effort search only
-//!
-
-use std::{env::current_exe, fmt, fs};
+#![forbid(unsafe_code)]
+#![doc = include_str!("../README.md")]
+use std::{env::current_exe, error::Error, fmt, fs};
 
 use findshlibs::SharedLibrary;
 use symbolic_debuginfo::Function;
 use symbolic_demangle::{Demangle, DemangleOptions};
 
-/// Attempt to get the declaration site of the given function item type
+/// Attempt to get the declaration site of the function item type of the
+/// given value, using its type name. This is a trivial wrapper around
+/// [`declaration`], but may be easier to use since function item types
+/// cannot be named.
 ///
-/// This will (likely) return `None` for
+/// This will (probably) return `None` for non-function item types.
+///
+/// See also "Caveats" in the [module level documentation](crate).
+///
+/// The `functions` example in this crate demonstrates this API.
 pub fn declaration_of<T>(_: &T) -> Option<DeclarationSite> {
     declaration::<T>()
 }
 
+/// Attempt to get the declaration site of the function item type of the
+/// given type, using its type name. In most cases, you may want to use  
+/// [`declaration_of`], since function item types are unnameable.
+///
+/// This function uses [`declaration_by_name`] with the [type
+/// name](core::any::type_name) of the type parameter `T`.
+///
+/// This will (probably) return `None` for non-function item types.
+///
+/// See also "Caveats" in the [module level documentation](crate).
+///
+/// Note: This could be a feature provided by [`core::any`] (using a new
+/// intrinsic, returning [`&'static Location<'static>`](core::panic::Location)).
+/// However, that (currently) doesn't exist. If it did, it would be:
+/// - significantly faster
+/// - more correct for functions as it would provide a column.
+/// - support e.g. structs, unions
+/// - not able to be run dynamically as [`declaration_by_name`]
 pub fn declaration<T>() -> Option<DeclarationSite> {
     declaration_by_name(core::any::type_name::<T>())
 }
 
+/// Attempt to get the declaration site of a currently loaded function
+/// with the given (unmangled) name.
+///
+/// See also "Caveats" in the [module level documentation](crate).
 pub fn declaration_by_name(name: &str) -> Option<DeclarationSite> {
-    for_each_currently_loaded_rust_function(|demangled_name, function| {
+    for_some_currently_loaded_rust_functions(|demangled_name, function| {
         (demangled_name == name)
             .then(|| (&function).try_into().ok())
             .flatten()
     })
 }
 
-/// Run `callback` on each currently loaded function which can be demangled in the current context,
-/// See the caveats section on the [module level documentation](crate).
+/// Run `callback` on each currently loaded function which can be demangled in
+/// the current context, See the caveats section on the [module level
+/// documentation](crate).
 ///
-/// As this crate depends on [`symbolic_demangle`] using the `"rust"` feature, this may skip
-/// non-Rust functions which have debug information available.
-/// However, on platforms where mangling isn't used in debug files, such as Windows' pdb files,
-/// this may also call callback with non-Rust functions. Additionally, if [`symbolic_demangle`] is
-/// in your tree with any other demanglings supported, they may also be used.
-pub fn for_each_currently_loaded_rust_function<R>(
+/// As this crate depends on [`symbolic_demangle`] using the `"rust"` feature,
+/// this may skip non-Rust functions (e.g. from libc), despite them having debug
+/// information available. However, on platforms where mangling isn't used in
+/// debug files, such as Windows' pdb files, this may also call callback with
+/// non-Rust functions. Additionally, if [`symbolic_demangle`] is in the current
+/// dependency tree with any other demangling features enabled supported,
+/// they may also be used.
+///
+/// See also "Caveats" in the [module level documentation](crate).
+pub fn for_some_currently_loaded_rust_functions<R>(
     mut callback: impl FnMut(String, Function) -> Option<R>,
 ) -> Option<R> {
     let mut libraries = vec![];
-    // Each might take locks - make this as short as possible to not block backtraces in other threads.
+    // `each` might take locks - make this as short as possible to not block
+    // backtraces in other threads.
     findshlibs::TargetSharedLibrary::each(|library| {
         libraries.push((
             library.name().to_owned(),
             library.debug_name().map(ToOwned::to_owned),
         ));
     });
+    // Error handling:
+    // In these loops, if we get an error, we just try again with the next item
+    // We're not trying to be fancy here - again, this is a best effort search.
+    // If nothing works, the user should have a fallback, as explained in caveats.
     for (library_path, debug_path) in libraries {
         let path = if let Some(debug_path) = debug_path {
             debug_path.into()
@@ -78,6 +114,8 @@ pub fn for_each_currently_loaded_rust_function<R>(
             for function in session.functions() {
                 if let Ok(function) = function {
                     if let Some(demangled_name) =
+                        // We only demangle the name since `type_name` doesn't return the
+                        // signature
                         function.name.demangle(DemangleOptions::name_only())
                     {
                         match callback(demangled_name, function) {
@@ -92,10 +130,12 @@ pub fn for_each_currently_loaded_rust_function<R>(
     None
 }
 
-/// A source file location, obtained from a [`symbolic_debuginfo::Function`], using
-/// [`FunctionExt::declaration_site`].
+/// A source file location, obtained from a [`symbolic_debuginfo::Function`],
+/// using [`TryFrom`]/[`TryInto`].
 ///
-/// A type which can be used to
+/// Printing this type into a terminal will often allow it to act as a link into
+/// the source code (if the working directories line up and the terminal
+/// emulator supports this feature).
 pub struct DeclarationSite {
     pub file: String,
     pub line: u32,
@@ -107,14 +147,39 @@ impl fmt::Display for DeclarationSite {
     }
 }
 
-#[derive(Copy, Clone, Debug)]
-pub struct MissingLines;
+/// An error returned in the [`TryFrom`] impl for [`DeclarationSite`].
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub enum DeclarationSiteError {
+    /// The debug info for the function contains no source locations
+    MissingLines,
+}
 
+impl fmt::Display for DeclarationSiteError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            DeclarationSiteError::MissingLines => write!(
+                f,
+                "debug info contains no source locations for this function"
+            ),
+        }
+    }
+}
+
+impl Error for DeclarationSiteError {}
+
+/// Get the site of the first line of the function, according to the debug info.
+///
+/// # Errors
+///
+/// If the function's debug info has no source locations
 impl<'a> TryFrom<&Function<'a>> for DeclarationSite {
-    type Error = MissingLines;
+    type Error = DeclarationSiteError;
 
     fn try_from(value: &Function<'a>) -> Result<Self, Self::Error> {
-        let line = &value.lines.get(0).ok_or(MissingLines)?;
+        let line = &value
+            .lines
+            .get(0)
+            .ok_or(DeclarationSiteError::MissingLines)?;
         let file = line.file.path_str();
 
         Ok(DeclarationSite {
